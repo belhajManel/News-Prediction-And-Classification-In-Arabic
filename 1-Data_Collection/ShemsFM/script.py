18289e3f48c1e1d0fa4ae4913f5bfcbfdf0640ea
BELHAJ Manel <77749468+belhajManel@users.noreply.github.com>
2021-12-10 19:06:07 +0000

    JawhraFm and ShemsFm Data

---
+++ 1-Data_Collection/ShemsFM/script.py
@@ -0,0 +1,17 @@
+import json
+import os
+path_to_json = '.'
+json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+print(json_files)
+def merge_JsonFiles(filename):
+    result = list()
+    result_name = 'all_posts_shemsfm.json'
+
+    for f1 in filename:
+        with open(f1, 'r') as infile:
+            result.extend(json.load(infile))
+
+    with open(result_name, 'w') as output_file:
+        json.dump(result, output_file)
+
+merge_JsonFiles(json_files)
